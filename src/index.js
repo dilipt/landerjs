cff90960af90a01eac2d910bf248788bef2e630f
dilipt <dthomas84@gmail.com>
2018-06-12 22:33:10 +1000

    laughable addition of physics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-const component = () => {
-  const elm = document.createElement('div');
-  elm.innerHTML = 'Hello, from webpack!';
-  return elm;
-};
+import Matter from 'matter-js';
 
-document.body.appendChild(component());
+const { Engine, Render, World, Bodies } = Matter;
+
+const canvas = document.querySelector('#cv');
+const context = canvas.getContext('2d');
+
+const engine = Engine.create();
+const render = Render.create({
+  element: document.body,
+  engine,
+});
+
+const boxA = Bodies.rectangle(400, 200, 80, 80);
+const boxB = Bodies.rectangle(450, 50, 80, 80);
+const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+World.add(engine.world, [boxA, boxB, ground]);
+Engine.run(engine);
+Render.run(render);
